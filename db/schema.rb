c308e7e87ab4576515cb010cd622fc51f6068231
Meranda Lashock <merlashock@gmail.com>
2020-02-11 10:38:52 -0500

    remove sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_161159) do
+ActiveRecord::Schema.define(version: 2020_02_10_192739) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
